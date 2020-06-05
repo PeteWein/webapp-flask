5ec48b7f9c43739e952854487bd5c6330fe4ab1d
Peter Wein <peter.j.wein@gmail.com>
2020-06-04 20:41:22 -0400

    shell instead of yaml

---
+++ git-hooks/pre-commit.sh
@@ -0,0 +1,9 @@
+# this repo is replicated in the .git/hooks using the same name; this is for clarity
+#!/bin/sh
+
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
+if [ "$branch" = "master" ]; then
+  echo "You can't commit directly to master branch"
+  exit 1
+fi
